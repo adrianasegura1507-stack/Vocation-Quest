5082b615bad8b632a29d0edb1ad3e0029957c6b6
adrianasegura1507-stack <adriana.segura1507@gmail.com>
2026-09-14 07:34:30 -0500

    Actualiza página principal de Vocation Quest

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.auth import auth
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app.register_blueprint(auth)
 
 @app.route("/")
 def inicio():
-    return "Vocation Quest funcionando"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
